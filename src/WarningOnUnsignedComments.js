44a46ccf4e402124a8bab691bccc21a86ec755a6
Helder <he7d3r@gmail.com>
2012-08-21 00:15:25 +0000

    Warning on unsigned comments
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/WarningOnUnsignedComments.js
@@ -0,0 +1,30 @@
+/**
+ * Warning on unsigned comments
+ * @author: [[User:Helder.wiki]]
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/WarningOnUnsignedComments.js]] ([[File:User:Helder.wiki/Tools/WarningOnUnsignedComments.js]])
+ */
+/*jslint browser: true, white: true, devel: true*/
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+function validateComments() {
+	$('#wpSave').on('click', function (e) {
+		var $textBox = $('#wpTextbox1'),
+			curText = $textBox.val(),
+			signRequired = mw.config.get('wgNamespaceNumber') % 2 === 1
+				|| curText.indexOf(' (UTC)') !== -1,
+			hasSignature = curText.indexOf('~~' + '~~') !== -1,
+			unsignedText = 'Parece que esqueceu de assinar o seu comentário. Deseja salvar mesmo assim?';
+		if (signRequired && !hasSignature && !confirm(unsignedText)) {
+			$textBox.focus();
+			e.preventDefault();
+			console.debug('Não salvar');
+		}
+	});
+}
+if ($.inArray(mw.config.get('wgAction'), ['edit', 'submit']) !== -1) {
+	$(validateComments);
+}
+
+}( mediaWiki, jQuery ) );
