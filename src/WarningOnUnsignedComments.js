6a84caf011d3c06625d768b43580fc407610fa51
Helder <he7d3r@gmail.com>
2014-09-27 20:49:04 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/WarningOnUnsignedComments.js
+++ src/WarningOnUnsignedComments.js
@@ -4,25 +4,25 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function validateComments() {
-	$( '#wpSave' ).on( 'click', function ( e ) {
-		var $textBox = $( '#wpTextbox1' ),
-			curText = $textBox.val(),
-			signRequired = mw.config.get( 'wgNamespaceNumber' ) % 2 === 1
-				|| curText.indexOf( ' (' + 'UTC)' ) !== -1,
-			hasSignature = /~{4}(?!<\/nowiki>)/.test( curText ),
-			unsignedText = 'Parece que esqueceu de colocar "~~' + '~~" no final da mensagem para assinar o seu comentário. Deseja salvar mesmo assim?';
-		if ( signRequired && !hasSignature && !confirm( unsignedText ) ) {
-			$textBox.focus();
-			e.preventDefault();
-			mw.log( 'Não salvar' );
-		}
-	} );
-}
-if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
-	$( validateComments );
-}
+	function validateComments() {
+		$( '#wpSave' ).on( 'click', function ( e ) {
+			var $textBox = $( '#wpTextbox1' ),
+				curText = $textBox.val(),
+				signRequired = mw.config.get( 'wgNamespaceNumber' ) % 2 === 1
+					|| curText.indexOf( ' (' + 'UTC)' ) !== -1,
+				hasSignature = /~{4}(?!<\/nowiki>)/.test( curText ),
+				unsignedText = 'Parece que esqueceu de colocar "~~' + '~~" no final da mensagem para assinar o seu comentário. Deseja salvar mesmo assim?';
+			if ( signRequired && !hasSignature && !confirm( unsignedText ) ) {
+				$textBox.focus();
+				e.preventDefault();
+				mw.log( 'Não salvar' );
+			}
+		} );
+	}
+	if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
+		$( validateComments );
+	}
 
 }( mediaWiki, jQuery ) );
